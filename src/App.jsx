35d1bab3154ca0d298e423d8346682aeb74eae0f
Abhay Sharma <akankshasharma47380@gmail.com>
2024-07-13 02:28:03 +0530

    v1.0 done succesfully

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import { Toaster } from "react-hot-toast";
+
+export default function App() {
+  return (
+    <>
+      <div>
+        {" "}
+        <Navbar />
+        <Home />
+        <About />
+        <Portfolio />
+        <Experience />
+        <Contact />
+        <Footer />
+      </div>
+      <Toaster />
+    </>
+  );
+}
